9bf40b643859fc8c763e6ae57ec5bfc4026cd963
m.rafat <iquixe@gmail.com>
2024-02-07 13:40:31 +0200

    add visa & visa percent properties

--- Invoice.cs
+++ Invoice.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace TodoApi {
+namespace TodoApi
+{
 
-    public class Invoice {//: IParsable<Invoice>
+    public class Invoice
+    {//: IParsable<Invoice>
         private const int MaxLineLength = 35;
         public string? PrinterName { get; set; }
         public string? TemplateName { get; set; }
@@ -33,17 +35,20 @@ namespace TodoApi {
 
 
         // uses reflection to retrieve a self-value by name
-        private string? GetValue(string propertyName) {
+        private string? GetValue(string propertyName)
+        {
             Type type = typeof(Invoice);
             System.Reflection.PropertyInfo? info = type.GetProperty(propertyName);
             return (string?)info?.GetValue(this, null)?.ToString();
         }
-        private int parseint(string str) {
+        private int parseint(string str)
+        {
             int val = 0;
             Int32.TryParse(str, out val);
             return val;
         }
-        private int sumCount(List<Item> list) {
+        private int sumCount(List<Item> list)
+        {
             // return list.Aggregate(0, (p, c) => p + parseint(c.Count ?? ""));
             int res = 0;
             foreach (var item in list)
@@ -53,17 +58,22 @@ namespace TodoApi {
 
         }
 
-        private List<string> SplitLongValueLines(string? value) {
+        private List<string> SplitLongValueLines(string? value)
+        {
             if (value == null || value.Trim().Length == 0) return new List<string> { "" };
             var line = "";
             var lines = new List<string>();
             var _value = value.Replace(Environment.NewLine, " — ").Replace("\n", " — ");
             // Console.WriteLine(_value);
             // string[] nm = words.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
-            foreach (var wd in _value.Split(" ")) {
-                if (!wd.Contains("—") && line.Length + wd.Length <= MaxLineLength) {
+            foreach (var wd in _value.Split(" "))
+            {
+                if (!wd.Contains("—") && line.Length + wd.Length <= MaxLineLength)
+                {
                     line += $" {wd.Trim()}";
-                } else {
+                }
+                else
+                {
                     lines.Add(line.Trim());
                     line = wd.Replace("—", "").Trim();
                 }
@@ -74,15 +84,19 @@ namespace TodoApi {
             return lines;
         }
         // converts a some properties to an iterable for MiniExcel
-        private List<Entry> GetPropertyListEntries(Dictionary<string, string> properties, List<string> multilineProperties) {
+        private List<Entry> GetPropertyListEntries(Dictionary<string, string> properties, List<string> multilineProperties)
+        {
             var entries = new List<Entry>();
 
-            foreach (KeyValuePair<string, string> prop in properties) {
+            foreach (KeyValuePair<string, string> prop in properties)
+            {
                 var value = GetValue(prop.Key);
                 if (value == null || value.Trim().Length == 0) continue;
 
-                if (!multilineProperties.Contains(prop.Key)) {
-                    entries.Add(new Entry() {
+                if (!multilineProperties.Contains(prop.Key))
+                {
+                    entries.Add(new Entry()
+                    {
                         Title = prop.Value,
                         Value = value,
                         // Values = new List<string?> { value }
@@ -93,8 +107,10 @@ namespace TodoApi {
                 var lines = SplitLongValueLines(value);
 
                 // TODO_BESAFE if sent is null
-                for (var i = 0; i < lines.Count; i++) {
-                    entries.Add(new Entry() {
+                for (var i = 0; i < lines.Count; i++)
+                {
+                    entries.Add(new Entry()
+                    {
                         Title = i == 0 ? prop.Value : null,
                         Value = lines[i],
                         // Values = new List<string?> { lines[i] }
@@ -108,8 +124,10 @@ namespace TodoApi {
 
 
 
-        public List<Entry> ClientInfo {
-            get {
+        public List<Entry> ClientInfo
+        {
+            get
+            {
                 var properties = new Dictionary<string, string>(){
                  {"ClientName","عميل"},
                  {"ClientPhone1","موبايل"},
@@ -123,40 +141,50 @@ namespace TodoApi {
         }
 
         public string? DeliveryName { get; set; }
-        public string? DeliveryNameTitle {
-            get {
+        public string? DeliveryNameTitle
+        {
+            get
+            {
                 return (DeliveryName != null && DeliveryName?.Length > 0) ? "طيار" : null;
             }
         }
 
         public List<Item> Items { get; set; } = new List<Item>();
-        public string? ItemsCountInfo {
-            get {
+        public string? ItemsCountInfo
+        {
+            get
+            {
                 if (Items == null || Items!.Count == 0) return null;
                 return $"مجموع الأصناف [ {sumCount(Items)} ]";
             }
         }
 
-        public List<Entry> ItemsInfo {
-            get {
+        public List<Entry> ItemsInfo
+        {
+            get
+            {
                 var entries = new List<Entry>();
 
-                foreach (var item in Items) {
+                foreach (var item in Items)
+                {
                     var lines = SplitLongValueLines(item?.Title ?? "");
 
                     // TODO_BESAFE if sent is null
                     for (var i = 0; i < lines.Count; i++)
-                        entries.Add(new Entry() {
+                        entries.Add(new Entry()
+                        {
                             Title = lines[i],
                             Value = i == 0 ? item?.Count : null,
                             // Values = new List<string?> { lines[i] }
                         });
 
 
-                    if (item?.Note != null && item?.Note?.Trim().Length > 0) {
+                    if (item?.Note != null && item?.Note?.Trim().Length > 0)
+                    {
                         lines = SplitLongValueLines(item?.Note ?? "");
                         for (var i = 0; i < lines.Count; i++)
-                            entries.Add(new Entry() {
+                            entries.Add(new Entry()
+                            {
                                 Title = $" —{lines[i]}—",
                             });
 
@@ -168,33 +196,43 @@ namespace TodoApi {
         }
 
         public List<Item> EditedItems { get; set; } = new List<Item>();
-        public string? EditedItemsCountInfo {
-            get {
+        public string? EditedItemsCountInfo
+        {
+            get
+            {
                 if (EditedItems == null || EditedItems!.Count == 0) return null;
                 return $"مجموع الأصناف المعدلة [ {sumCount(EditedItems)} ]";
             }
         }
-        public string? EditedTitle {
-            get {
+        public string? EditedTitle
+        {
+            get
+            {
                 return (EditedItems != null && EditedItems?.Count > 0) ? "تعديل" : null;
             }
         }
-        public List<Entry> EditedItemsInfo {
-            get {
+        public List<Entry> EditedItemsInfo
+        {
+            get
+            {
                 var entries = new List<Entry>();
 
-                foreach (var item in EditedItems) {
+                foreach (var item in EditedItems)
+                {
                     var lines = SplitLongValueLines(item?.Title ?? "");
                     for (var i = 0; i < lines.Count; i++)
-                        entries.Add(new Entry() {
+                        entries.Add(new Entry()
+                        {
                             Title = lines[i],
                             Value = i == 0 ? item?.Count : null,
                         });
 
-                    if (item?.Note != null && item?.Note?.Trim().Length > 0) {
+                    if (item?.Note != null && item?.Note?.Trim().Length > 0)
+                    {
                         lines = SplitLongValueLines(item?.Note ?? "");
                         for (var i = 0; i < lines.Count; i++)
-                            entries.Add(new Entry() {
+                            entries.Add(new Entry()
+                            {
                                 Title = $" —{lines[i]}—",
                             });
                     }
@@ -206,21 +244,27 @@ namespace TodoApi {
 
 
         public List<Item> OtherKitchensItems { get; set; } = new List<Item>();
-        public string? OtherKitchensItemsCountInfo {
-            get {
+        public string? OtherKitchensItemsCountInfo
+        {
+            get
+            {
                 if (OtherKitchensItems == null || OtherKitchensItems!.Count == 0) return null;
                 return $"مجموع الأصناف الباقية [ {sumCount(OtherKitchensItems)} ]";
             }
         }
-        public List<Entry> OtherKitchensItemsInfo {
-            get {
+        public List<Entry> OtherKitchensItemsInfo
+        {
+            get
+            {
                 var entries = new List<Entry>();
 
-                foreach (var item in OtherKitchensItems) {
+                foreach (var item in OtherKitchensItems)
+                {
                     var lines = SplitLongValueLines(item?.Title ?? "");
 
                     for (var i = 0; i < lines.Count; i++)
-                        entries.Add(new Entry() {
+                        entries.Add(new Entry()
+                        {
                             Title = lines[i],
                             Value = i == 0 ? item?.Count : null,
                         });
@@ -230,23 +274,29 @@ namespace TodoApi {
             }
         }
         public List<string> OtherKitchens { get; set; } = new List<string>();
-        public string? OtherKitchensTitle {
-            get {
+        public string? OtherKitchensTitle
+        {
+            get
+            {
                 if (OtherKitchens == null || OtherKitchens?.Count == 0) return null;
                 if (OtherKitchensItems == null || OtherKitchensItems?.Count == 0) return null;
                 string kitchenNames = String.Join("و ", OtherKitchens!.ToArray());
                 return $"يوجد بقية للطلب في [ {kitchenNames} ]";
             }
         }
-        public List<Entry> OtherKitchensInfo {
-            get {
+        public List<Entry> OtherKitchensInfo
+        {
+            get
+            {
                 var ret = new List<Entry>();
 
                 if (OtherKitchensTitle == null || OtherKitchensTitle?.Length == 0) return ret;
                 if (OtherKitchens == null || OtherKitchens?.Count == 0) return ret;
 
-                foreach (var item in SplitLongValueLines(OtherKitchensTitle!)) {
-                    ret.Add(new Entry() {
+                foreach (var item in SplitLongValueLines(OtherKitchensTitle!))
+                {
+                    ret.Add(new Entry()
+                    {
                         Title = item,
                     });
                 }
@@ -257,15 +307,19 @@ namespace TodoApi {
 
         public string? SectionName { get; set; }
         public string? ScheduleTime { get; set; }
-        public string? ScheduleTitle {
-            get {
+        public string? ScheduleTitle
+        {
+            get
+            {
                 return (ScheduleTime != null && ScheduleTime?.Length > 0) ? "حجز" : null;
             }
         }
 
         public string? TableNo { get; set; }
-        public string? TableTitle {
-            get {
+        public string? TableTitle
+        {
+            get
+            {
                 return (TableNo != null && TableNo?.Length > 0) ? "طاولة" : null;
             }
         }
@@ -277,10 +331,19 @@ namespace TodoApi {
         public string? Service { get; set; }
         public string? Delivery { get; set; }
         public string? Vat { get; set; }
+        public string? Visa { get; set; }
+        public string? VisaPer { get; set; }
+        public string? VisaPerTitle{
+            get{
+                return (VisaPer != null && VisaPer?.Length > 0)? $"Visa {VisaPer} %" : null;
+            }
+        }
         public string? Total { get; set; }
 
-        public List<Entry> InvoiceInfo {
-            get {
+        public List<Entry> InvoiceInfo
+        {
+            get
+            {
                 var properties = new Dictionary<string, string>(){
                  {"Discount","الخصم"},
                  {"Service","Service 12%"},
@@ -304,7 +367,8 @@ namespace TodoApi {
 
 
 
-    internal class DateTimeConverter : JsonConverter<DateTime> {
+    internal class DateTimeConverter : JsonConverter<DateTime>
+    {
 
         public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
             => JsonSerializer.Deserialize<DateTime>(ref reader, options);
@@ -313,7 +377,8 @@ namespace TodoApi {
         => writer.WriteStringValue(value.ToString()); //writer.WriteStringValue(value);
     }
 
-    public class Entry {
+    public class Entry
+    {
         // [JsonConverter(typeof(StringConverter))]
         public string? Title { get; set; }
         // public List<string?> Values { get; set; } = new List<string?>();
@@ -321,7 +386,8 @@ namespace TodoApi {
         public string? Value2 { get; set; }
         public string? Value3 { get; set; }
     }
-    public class Item {
+    public class Item
+    {
         // [JsonConverter(typeof(StringConverter))]
         public string? Title { get; set; }
         public string? Count { get; set; }
