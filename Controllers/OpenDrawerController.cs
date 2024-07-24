d3c85010f4ee78d11419442dde3212364545207b
mrt <iquixe@gmail.com>
2024-07-24 14:11:04 +0300

    add dotnet 5 compatibility imports

--- Controllers/OpenDrawerController.cs
+++ Controllers/OpenDrawerController.cs
@@ -1,33 +1,31 @@
 
 using Microsoft.AspNetCore.Mvc;
 
-namespace TodoApi.Controllers;
+namespace TodoApi.Controllers {
 
-[ApiController]
-[Route("[controller]")]
-public class OpenDrawerController : ControllerBase {
+  [ApiController]
+  [Route("[controller]")]
+  public class OpenDrawerController : ControllerBase {
 
-  private readonly ILogger<PrintingDataController> _logger;
 
-  public OpenDrawerController(ILogger<PrintingDataController> logger) {
-    _logger = logger;
-  }
+    public OpenDrawerController() { }
 
-  [HttpGet(Name = "TestOpenDrawer")]
-  public dynamic Get() {
-    return Ok("Success");
-  }
+    [HttpGet(Name = "TestOpenDrawer")]
+    public dynamic Get() {
+      return Ok("Success");
+    }
 
-  [HttpPost(Name = "PostOpenDrawer")]
-  public dynamic Post(string printerName) {
-    const string ESC = "\u001B";
-    const string p = "\u0070";
-    const string m = "\u0000";
-    const string t1 = "\u0025";
-    const string t2 = "\u0250";
-    const string openTillCommand = ESC + p + m + t1 + t2;
+    [HttpPost(Name = "PostOpenDrawer")]
+    public dynamic Post(string printerName) {
+      const string ESC = "\u001B";
+      const string p = "\u0070";
+      const string m = "\u0000";
+      const string t1 = "\u0025";
+      const string t2 = "\u0250";
+      const string openTillCommand = ESC + p + m + t1 + t2;
 
-    RawPrinterHelper.SendStringToPrinter(printerName, openTillCommand);
-    return Ok();
+      RawPrinterHelper.SendStringToPrinter(printerName, openTillCommand);
+      return Ok();
+    }
   }
 }
