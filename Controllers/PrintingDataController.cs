1d83b6a45a271966eeeeda5a59fc44223b80a5e7
m.rafat <iquixe@gmail.com>
2024-10-16 10:38:32 +0200

    .

--- Controllers/PrintingDataController.cs
+++ Controllers/PrintingDataController.cs
@@ -1,105 +1,125 @@
 using System;
-using System.Globalization;
 using System.Diagnostics;
+using System.Globalization;
 using Microsoft.AspNetCore.Mvc;
 using MiniExcelLibs;
 using MiniExcelLibs.OpenXml;
+
 namespace TodoApi.Controllers;
 
 [ApiController]
 [Route("[controller]")]
-public class PrintingDataController : ControllerBase {
+public class PrintingDataController : ControllerBase
+{
+    private readonly ILogger<PrintingDataController> _logger;
 
-  private readonly ILogger<PrintingDataController> _logger;
-
-  public PrintingDataController(ILogger<PrintingDataController> logger) {
-    _logger = logger;
-  }
-
-  [HttpGet(Name = "TestPrintingData")]
-  public dynamic Get() {
-    return Ok("Success");
-  }
-
-  [HttpPost(Name = "PostPrintingData")]
-  public dynamic Post([FromBody] Invoice invoice) {
-
-    var culture = new CultureInfo("ar-EG");
-    var date = DateTime.Parse(invoice.Date);
-
-    string year = date.Year.ToString();
-    string month = $"{date.Month.ToString()} — {date.ToString("MMMM", culture)}";
-    string day = $"{date.Day.ToString()} — {date.ToString("dddd", culture)}";
-
-
-    string outputfile = $"{invoice.Date} #{invoice.InvoiceNo} — {Guid.NewGuid()}.xlsx";
-
-    string folderPath = Path.Combine(Environment.CurrentDirectory, "printer", "out", year, month, day, $"فاتورة — {invoice.InvoiceNo}");
-    if (!Directory.Exists(folderPath)) {
-      Directory.CreateDirectory(folderPath);
+    public PrintingDataController(ILogger<PrintingDataController> logger)
+    {
+        _logger = logger;
     }
-    string outputPath = Path.Combine(folderPath, outputfile);
 
-    string inputPath = Path.Combine(Environment.CurrentDirectory, "printer", "templates", $"{invoice.TemplateName ?? ""}.xlsx");
-
-    var config = new OpenXmlConfiguration() {
-      IgnoreTemplateParameterMissing = true,
-      FillMergedCells = true,
-      EnableWriteNullValueCell = true
-    };
-
-    try {
-      Console.WriteLine("creating file");
-
-      MiniExcel.SaveAsByTemplate(@outputPath, @inputPath, invoice, configuration: config);
-
-      // TODO: check if required printer exists otherwise it sends to default
-      // TODO: clean up files after sending to printer
-      // TODO: check if template && lib files exists
-
-      var result = SendXlsx2PrinterByInterop(outputPath, invoice.PrinterName);
-
-      if (result.Trim() == "")
-        return Ok();
-      return StatusCode(StatusCodes.Status500InternalServerError, result);
-
-    } catch (Exception e) {
-      Console.WriteLine(e.Message);
-      var err = $"message = {e.Message}, stack = {e.StackTrace}";
-      return StatusCode(StatusCodes.Status500InternalServerError, err);
+    [HttpGet(Name = "TestPrintingData")]
+    public dynamic Get()
+    {
+        return Ok("Printing Data Success");
     }
-  }
 
-  private string SendXlsx2PrinterByInterop(string filePath, string? printerName) {
-    string result = "";
-    using (var process = new Process()) {
-      var path = Path.GetFullPath(Path.Combine(Environment.CurrentDirectory, "printer", "lib", "printer.exe"));
-      process.StartInfo.FileName = path;
-      process.StartInfo.Arguments = $"\"{filePath}\" \"{printerName}\"";
-      process.StartInfo.CreateNoWindow = true;
-      process.StartInfo.UseShellExecute = false;
-      process.StartInfo.RedirectStandardOutput = true;
-      process.StartInfo.RedirectStandardError = true;
+    [HttpPost(Name = "PostPrintingData")]
+    public dynamic Post([FromBody] Invoice invoice)
+    {
+        var culture = new CultureInfo("ar-EG");
+        var date = DateTime.Parse(invoice.Date);
 
+        string year = date.Year.ToString();
+        string month = $"{date.Month.ToString()} — {date.ToString("MMMM", culture)}";
+        string day = $"{date.Day.ToString()} — {date.ToString("dddd", culture)}";
 
-      process.OutputDataReceived += (sender, data) => result += data.Data ?? "";
-      process.ErrorDataReceived += (sender, data) => result += data.Data ?? "";
-      Console.WriteLine("starting to print");
-      process.Start();
-      process.BeginOutputReadLine();
-      process.BeginErrorReadLine();
-      do {
-        if (!process.HasExited) {
-          // Refresh the current process property values.  
-          process.Refresh();
-          // Console.WriteLine($"exit {process.HasExited}, result: {result}");
+        string outputfile = $"{invoice.Date} #{invoice.InvoiceNo} — {Guid.NewGuid()}.xlsx";
+
+        string folderPath = Path.Combine(
+            Environment.CurrentDirectory,
+            "printer",
+            "out",
+            year,
+            month,
+            day,
+            $"فاتورة — {invoice.InvoiceNo}"
+        );
+        if (!Directory.Exists(folderPath))
+        {
+            Directory.CreateDirectory(folderPath);
         }
-      }
-      while (!process.WaitForExit(500));
+        string outputPath = Path.Combine(folderPath, outputfile);
 
+        string inputPath = Path.Combine(
+            Environment.CurrentDirectory,
+            "printer",
+            "templates",
+            $"{invoice.TemplateName ?? ""}.xlsx"
+        );
+
+        var config = new OpenXmlConfiguration()
+        {
+            IgnoreTemplateParameterMissing = true,
+            FillMergedCells = true,
+            EnableWriteNullValueCell = true
+        };
+
+        try
+        {
+            Console.WriteLine("creating file");
+
+            MiniExcel.SaveAsByTemplate(@outputPath, @inputPath, invoice, configuration: config);
+
+            // TODO: check if required printer exists otherwise it sends to default
+            // TODO: clean up files after sending to printer
+            // TODO: check if template && lib files exists
+
+            //   var result = SendXlsx2PrinterByInterop(outputPath, invoice.PrinterName);
+
+            //   if (result.Trim() == "")
+            return Ok();
+            //  return StatusCode(StatusCodes.Status500InternalServerError, result);
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+            var err = $"message = {e.Message}, stack = {e.StackTrace}";
+            return StatusCode(StatusCodes.Status500InternalServerError, err);
+        }
     }
-    return result;
-  }
 
+    private string SendXlsx2PrinterByInterop(string filePath, string? printerName)
+    {
+        string result = "";
+        using (var process = new Process())
+        {
+            var path = Path.GetFullPath(
+                Path.Combine(Environment.CurrentDirectory, "printer", "lib", "printer.exe")
+            );
+            process.StartInfo.FileName = path;
+            process.StartInfo.Arguments = $"\"{filePath}\" \"{printerName}\"";
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
 
+            process.OutputDataReceived += (sender, data) => result += data.Data ?? "";
+            process.ErrorDataReceived += (sender, data) => result += data.Data ?? "";
+            Console.WriteLine("starting to print");
+            process.Start();
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+            do
+            {
+                if (!process.HasExited)
+                {
+                    // Refresh the current process property values.
+                    process.Refresh();
+                    // Console.WriteLine($"exit {process.HasExited}, result: {result}");
+                }
+            } while (!process.WaitForExit(500));
+        }
+        return result;
+    }
 }
--- Invoice.cs
+++ Invoice.cs
@@ -1,4 +1,3 @@
-
 using System;
 using System.Collections.Generic;
 using System.Text.Json;
@@ -6,9 +5,8 @@ using System.Text.Json.Serialization;
 
 namespace TodoApi
 {
-
     public class Invoice
-    {//: IParsable<Invoice>
+    { //: IParsable<Invoice>
         private const int MaxLineLength = 35;
         public string? PrinterName { get; set; }
         public string? TemplateName { get; set; }
@@ -18,7 +16,6 @@ namespace TodoApi
         public string? Branch { get; set; }
         public string? BranchDesc { get; set; }
 
-
         public string? Date { get; set; }
         public string? Time { get; set; }
 
@@ -26,13 +23,11 @@ namespace TodoApi
 
         public string? InvoiceType { get; set; }
 
-
         public string? ClientName { get; set; }
         public string? ClientPhone1 { get; set; }
         public string? ClientPhone2 { get; set; }
         public string? ClientAddress { get; set; }
         public string? ClientArea { get; set; }
-
 
         // uses reflection to retrieve a self-value by name
         private string? GetValue(string propertyName)
@@ -41,12 +36,21 @@ namespace TodoApi
             System.Reflection.PropertyInfo? info = type.GetProperty(propertyName);
             return (string?)info?.GetValue(this, null)?.ToString();
         }
+
         private int parseint(string str)
         {
             int val = 0;
             Int32.TryParse(str, out val);
             return val;
         }
+
+        private decimal parseDecimal(string str)
+        {
+            decimal val = 0M;
+            Decimal.TryParse(str, out val);
+            return val;
+        }
+
         private int sumCount(List<Item> list)
         {
             // return list.Aggregate(0, (p, c) => p + parseint(c.Count ?? ""));
@@ -55,12 +59,21 @@ namespace TodoApi
                 res += parseint(item.Count ?? "0");
 
             return res;
+        }
 
+        private decimal sumTotal(List<Item> list)
+        {
+            decimal res = 0M;
+            foreach (var item in list)
+                res += parseDecimal(item.TotalPrice ?? "0.0");
+
+            return res;
         }
 
         private List<string> SplitLongValueLines(string? value)
         {
-            if (value == null || value.Trim().Length == 0) return new List<string> { "" };
+            if (value == null || value.Trim().Length == 0)
+                return new List<string> { "" };
             var line = "";
             var lines = new List<string>();
             var _value = value.Replace(Environment.NewLine, " — ").Replace("\n", " — ");
@@ -77,30 +90,38 @@ namespace TodoApi
                     lines.Add(line.Trim());
                     line = wd.Replace("—", "").Trim();
                 }
-            };
+            }
+            ;
             if (line.Trim().Length > 0)
                 lines.Add(line.Trim());
 
             return lines;
         }
+
         // converts a some properties to an iterable for MiniExcel
-        private List<Entry> GetPropertyListEntries(Dictionary<string, string> properties, List<string> multilineProperties)
+        private List<Entry> GetPropertyListEntries(
+            Dictionary<string, string> properties,
+            List<string> multilineProperties
+        )
         {
             var entries = new List<Entry>();
 
             foreach (KeyValuePair<string, string> prop in properties)
             {
                 var value = GetValue(prop.Key);
-                if (value == null || value.Trim().Length == 0) continue;
+                if (value == null || value.Trim().Length == 0)
+                    continue;
 
                 if (!multilineProperties.Contains(prop.Key))
                 {
-                    entries.Add(new Entry()
-                    {
-                        Title = prop.Value,
-                        Value = value,
-                        // Values = new List<string?> { value }
-                    });
+                    entries.Add(
+                        new Entry()
+                        {
+                            Title = prop.Value,
+                            Value = value,
+                            // Values = new List<string?> { value }
+                        }
+                    );
                     continue;
                 }
 
@@ -109,32 +130,32 @@ namespace TodoApi
                 // TODO_BESAFE if sent is null
                 for (var i = 0; i < lines.Count; i++)
                 {
-                    entries.Add(new Entry()
-                    {
-                        Title = i == 0 ? prop.Value : null,
-                        Value = lines[i],
-                        // Values = new List<string?> { lines[i] }
-                    });
+                    entries.Add(
+                        new Entry()
+                        {
+                            Title = i == 0 ? prop.Value : null,
+                            Value = lines[i],
+                            // Values = new List<string?> { lines[i] }
+                        }
+                    );
                 }
-
             }
 
             return entries;
         }
 
-
-
         public List<Entry> ClientInfo
         {
             get
             {
-                var properties = new Dictionary<string, string>(){
-                 {"ClientName","عميل"},
-                 {"ClientPhone1","موبايل"},
-                 {"ClientPhone2","موبايل آخر"},
-                 {"ClientAddress","عنوان"},
-                 {"ClientArea", "منطقة"}
-            };
+                var properties = new Dictionary<string, string>()
+                {
+                    { "ClientName", "عميل" },
+                    { "ClientPhone1", "موبايل" },
+                    { "ClientPhone2", "موبايل آخر" },
+                    { "ClientAddress", "عنوان" },
+                    { "ClientArea", "منطقة" }
+                };
 
                 return GetPropertyListEntries(properties, new List<string> { "ClientAddress" });
             }
@@ -143,10 +164,7 @@ namespace TodoApi
         public string? DeliveryName { get; set; }
         public string? DeliveryNameTitle
         {
-            get
-            {
-                return (DeliveryName != null && DeliveryName?.Length > 0) ? "طيار" : null;
-            }
+            get { return (DeliveryName != null && DeliveryName?.Length > 0) ? "طيار" : null; }
         }
 
         public List<Item> Items { get; set; } = new List<Item>();
@@ -154,7 +172,8 @@ namespace TodoApi
         {
             get
             {
-                if (Items == null || Items!.Count == 0) return null;
+                if (Items == null || Items!.Count == 0)
+                    return null;
                 return $"مجموع الأصناف [ {sumCount(Items)} ]";
             }
         }
@@ -171,23 +190,20 @@ namespace TodoApi
 
                     // TODO_BESAFE if sent is null
                     for (var i = 0; i < lines.Count; i++)
-                        entries.Add(new Entry()
-                        {
-                            Title = lines[i],
-                            Value = i == 0 ? item?.Count : null,
-                            // Values = new List<string?> { lines[i] }
-                        });
-
+                        entries.Add(
+                            new Entry()
+                            {
+                                Title = lines[i],
+                                Value = i == 0 ? item?.Count : null,
+                                // Values = new List<string?> { lines[i] }
+                            }
+                        );
 
                     if (item?.Note != null && item?.Note?.Trim().Length > 0)
                     {
                         lines = SplitLongValueLines(item?.Note ?? "");
                         for (var i = 0; i < lines.Count; i++)
-                            entries.Add(new Entry()
-                            {
-                                Title = $" —{lines[i]}—",
-                            });
-
+                            entries.Add(new Entry() { Title = $" —{lines[i]}—", });
                     }
                 }
 
@@ -200,16 +216,14 @@ namespace TodoApi
         {
             get
             {
-                if (EditedItems == null || EditedItems!.Count == 0) return null;
+                if (EditedItems == null || EditedItems!.Count == 0)
+                    return null;
                 return $"مجموع الأصناف المعدلة [ {sumCount(EditedItems)} ]";
             }
         }
         public string? EditedTitle
         {
-            get
-            {
-                return (EditedItems != null && EditedItems?.Count > 0) ? "تعديل" : null;
-            }
+            get { return (EditedItems != null && EditedItems?.Count > 0) ? "تعديل" : null; }
         }
         public List<Entry> EditedItemsInfo
         {
@@ -221,20 +235,15 @@ namespace TodoApi
                 {
                     var lines = SplitLongValueLines(item?.Title ?? "");
                     for (var i = 0; i < lines.Count; i++)
-                        entries.Add(new Entry()
-                        {
-                            Title = lines[i],
-                            Value = i == 0 ? item?.Count : null,
-                        });
+                        entries.Add(
+                            new Entry() { Title = lines[i], Value = i == 0 ? item?.Count : null, }
+                        );
 
                     if (item?.Note != null && item?.Note?.Trim().Length > 0)
                     {
                         lines = SplitLongValueLines(item?.Note ?? "");
                         for (var i = 0; i < lines.Count; i++)
-                            entries.Add(new Entry()
-                            {
-                                Title = $" —{lines[i]}—",
-                            });
+                            entries.Add(new Entry() { Title = $" —{lines[i]}—", });
                     }
                 }
 
@@ -242,13 +251,13 @@ namespace TodoApi
             }
         }
 
-
         public List<Item> OtherKitchensItems { get; set; } = new List<Item>();
         public string? OtherKitchensItemsCountInfo
         {
             get
             {
-                if (OtherKitchensItems == null || OtherKitchensItems!.Count == 0) return null;
+                if (OtherKitchensItems == null || OtherKitchensItems!.Count == 0)
+                    return null;
                 return $"مجموع الأصناف الباقية [ {sumCount(OtherKitchensItems)} ]";
             }
         }
@@ -263,11 +272,9 @@ namespace TodoApi
                     var lines = SplitLongValueLines(item?.Title ?? "");
 
                     for (var i = 0; i < lines.Count; i++)
-                        entries.Add(new Entry()
-                        {
-                            Title = lines[i],
-                            Value = i == 0 ? item?.Count : null,
-                        });
+                        entries.Add(
+                            new Entry() { Title = lines[i], Value = i == 0 ? item?.Count : null, }
+                        );
                 }
 
                 return entries;
@@ -278,8 +285,10 @@ namespace TodoApi
         {
             get
             {
-                if (OtherKitchens == null || OtherKitchens?.Count == 0) return null;
-                if (OtherKitchensItems == null || OtherKitchensItems?.Count == 0) return null;
+                if (OtherKitchens == null || OtherKitchens?.Count == 0)
+                    return null;
+                if (OtherKitchensItems == null || OtherKitchensItems?.Count == 0)
+                    return null;
                 string kitchenNames = String.Join("و ", OtherKitchens!.ToArray());
                 return $"يوجد بقية للطلب في [ {kitchenNames} ]";
             }
@@ -290,15 +299,14 @@ namespace TodoApi
             {
                 var ret = new List<Entry>();
 
-                if (OtherKitchensTitle == null || OtherKitchensTitle?.Length == 0) return ret;
-                if (OtherKitchens == null || OtherKitchens?.Count == 0) return ret;
+                if (OtherKitchensTitle == null || OtherKitchensTitle?.Length == 0)
+                    return ret;
+                if (OtherKitchens == null || OtherKitchens?.Count == 0)
+                    return ret;
 
                 foreach (var item in SplitLongValueLines(OtherKitchensTitle!))
                 {
-                    ret.Add(new Entry()
-                    {
-                        Title = item,
-                    });
+                    ret.Add(new Entry() { Title = item, });
                 }
 
                 return ret;
@@ -309,23 +317,14 @@ namespace TodoApi
         public string? ScheduleTime { get; set; }
         public string? ScheduleTitle
         {
-            get
-            {
-                return (ScheduleTime != null && ScheduleTime?.Length > 0) ? "حجز" : null;
-            }
+            get { return (ScheduleTime != null && ScheduleTime?.Length > 0) ? "حجز" : null; }
         }
 
         public string? TableNo { get; set; }
         public string? TableTitle
         {
-            get
-            {
-                return (TableNo != null && TableNo?.Length > 0) ? "طاولة" : null;
-            }
+            get { return (TableNo != null && TableNo?.Length > 0) ? "طاولة" : null; }
         }
-
-
-
 
         public string? Discount { get; set; }
         public string? Service { get; set; }
@@ -333,24 +332,33 @@ namespace TodoApi
         public string? Vat { get; set; }
         public string? Visa { get; set; }
         public string? VisaPer { get; set; }
-        public string? VisaPerTitle{
-            get{
-                return (VisaPer != null && VisaPer?.Length > 0)? $"Visa {VisaPer} %" : null;
-            }
+        public string? VisaPerTitle
+        {
+            get { return (VisaPer != null && VisaPer?.Length > 0) ? $"Visa {VisaPer} %" : null; }
         }
         public string? Total { get; set; }
+        public decimal ItemsTotal
+        {
+            get { return sumTotal(Items); }
+        }
+        public decimal ItemsTax14
+        {
+            get { return Decimal.Multiply(ItemsTotal, 0.14M); }
+        }
 
         public List<Entry> InvoiceInfo
         {
             get
             {
-                var properties = new Dictionary<string, string>(){
-                 {"Discount","الخصم"},
-                 {"Service","Service 12%"},
-                 {"Delivery","خدمة توصيل"},
-                 {"Vat","Vat 14%"},
-                 {"Total", "اجمالي"}
-            };
+                var properties = new Dictionary<string, string>()
+                {
+                    { "Discount", "الخصم" },
+                    { "Service", "Service" },
+                    { "Delivery", "خدمة توصيل" },
+                    { "Vat", "Vat 14%" },
+                    { "Visa", VisaPerTitle },
+                    { "Total", "اجمالي" }
+                };
                 return GetPropertyListEntries(properties, new List<string> { "ClientAddress" });
             }
         }
@@ -362,30 +370,34 @@ namespace TodoApi
         public string? FooterNote1 { get; set; }
         public string? FooterNote2 { get; set; }
         public string? FooterNote3 { get; set; }
-
     }
-
-
 
     internal class DateTimeConverter : JsonConverter<DateTime>
     {
+        public override DateTime Read(
+            ref Utf8JsonReader reader,
+            Type typeToConvert,
+            JsonSerializerOptions options
+        ) => JsonSerializer.Deserialize<DateTime>(ref reader, options);
 
-        public override DateTime Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
-            => JsonSerializer.Deserialize<DateTime>(ref reader, options);
-
-        public override void Write(Utf8JsonWriter writer, DateTime value, JsonSerializerOptions options)
-        => writer.WriteStringValue(value.ToString()); //writer.WriteStringValue(value);
+        public override void Write(
+            Utf8JsonWriter writer,
+            DateTime value,
+            JsonSerializerOptions options
+        ) => writer.WriteStringValue(value.ToString()); //writer.WriteStringValue(value);
     }
 
     public class Entry
     {
         // [JsonConverter(typeof(StringConverter))]
         public string? Title { get; set; }
+
         // public List<string?> Values { get; set; } = new List<string?>();
         public string? Value { get; set; }
         public string? Value2 { get; set; }
         public string? Value3 { get; set; }
     }
+
     public class Item
     {
         // [JsonConverter(typeof(StringConverter))]
@@ -395,5 +407,4 @@ namespace TodoApi
         public string? TotalPrice { get; set; }
         public string? Note { get; set; }
     }
-
 }
